af93a74c4096ae5476fa571984b5d3b0bb7357f0
Bhuvan2023 <bhuvanshankarmct2019@gmail.com>
2023-12-13 07:49:28 +0530

    counter done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './style.css';
+import {useState} from "react"
 
 export default function App() {
+  let [age, setAge] = useState(0);
+let increment = ()=>{
+  setAge(age++)
+}
+let decrease = ()=>{
+  setAge(age--)
+}
   return <div>
-    
+    <p >{age}</p>
+    <button onClick={increment}>Add</button>
+    <button onClick={decrease}>sub</button>
+
   </div>;
 }
